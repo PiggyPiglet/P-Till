eee676eeaecfdeb8c4b3409206a654e96dc57ea4
PiggyPiglet <noreply@piggypiglet.me>
2020-10-04 22:48:19 +0800

    skeleton project

---
+++ src/main/java/me/piggypiglet/ptill/PtillApplication.java
@@ -0,0 +1,11 @@
+package me.piggypiglet.ptill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtillApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PtillApplication.class, args);
+    }
+}
---
+++ src/test/java/me/piggypiglet/ptill/PtillApplicationTests.java
@@ -0,0 +1,13 @@
+package me.piggypiglet.ptill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
